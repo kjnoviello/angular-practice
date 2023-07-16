0ba476204297804a582a1b6b4668d2d390a2d59e
kevin joel noviello <kjnoviello@gmail.com>
2023-07-16 01:00:40 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tarea';
+
+  array = [
+    {nombre: "El Señor de los Anillos", costo: "$ 1200"},
+    {nombre: "Juego de Tronos", costo: "$ 694"},
+    {nombre: "Harry Potter y la piedra filosofal", costo: "$ 223"},
+    {nombre: "Apocalipsis Zombie", costo: "$ 444",},
+    {nombre: "Dune", costo: "$ 412"},
+    {nombre: "Neuromante", costo: "$ 271"},
+
+
+  ];
+
+  showList = true;
+  toggle = (() => this.showList = !this.showList);
 }
